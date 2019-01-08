fb7274e359c37b086d0196fbacab9551bd82001b
shub0y <gadewarshubham002@gmail.com>
2019-01-08 11:14:31 -0500

    Modifing patch and delete

--- api/routes/products.js
+++ api/routes/products.js
@@ -3,13 +3,16 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const Product = require('../models/product');
 
-router.get('/', (req, res, next)=>{
-    Product
-    .find()
+router.get("/", (req, res, next) => { 
+    Product.find()
     .exec()
     .then(docs => {
         console.log(docs);
-        res.status(200).json(docs);
+        // if(docs >= 0 ){
+            res.status(200).json(docs);
+        // } else {
+        //     res.status(404).json({message: 'Nothing found'});
+        // }
     })
     .catch(err => {
         console.log(err);
@@ -64,14 +67,36 @@ router.get('/:productId', (req, res, next) => {
 });
 
 router.patch('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Updated the product'
-    });
+    const id = req.params.productId;
+    const updateOps = {};
+    for (const ops of req.body) {
+        updateOps[ops.propName] = ops.value;
+    }
+    Product.update({_id: id}, { $set: updateOps}).exec()
+    .then(result => {
+        console.log(result);
+        res.status(200).json(result);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        })
+    });;
 });
 
 router.delete('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Deleted the product'
+    const id = req.params.productId;
+    Product.remove({_id: id})
+    .exec()
+    .then(result => {
+        res.status(200).json(result);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        })
     });
 });
 
