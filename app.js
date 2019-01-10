50e222b4bbe8cb85ef5ed4037dbf869be1fb7d96
shub0y <gadewarshubham002@gmail.com>
2019-01-10 11:19:55 -0500

    Added users

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const userRoutes = require ('./api/routes/user');
 
 mongoose.connect('mongodb://shub0y:shub0y@node-rest-shop-shard-00-00-bafpf.mongodb.net:27017,node-rest-shop-shard-00-01-bafpf.mongodb.net:27017,node-rest-shop-shard-00-02-bafpf.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin&retryWrites=true', { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 
 app.use(morgan('dev'));
+app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
@@ -28,6 +30,7 @@ app.use((req, res, next) => {
 //routes should handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/user', userRoutes);
 
 app.use((req, res, next) =>{
     const error = new Error('Not Found');
